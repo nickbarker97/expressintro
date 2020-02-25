9cf4466b3964e6c11affd3f3b37d03dcbec3646b
(s) Nicholas Barker <nbarker1@pm7-0274.uopnet.plymouth.ac.uk>
2020-02-25 12:10:22 +0000

    inital commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res)=>{
+    res.send('This is the Home Page')
+});
+
+app.get('/about', (req, res)=>{
+    res.send('This is the About page')
+});
+
+
+
+//Listening for request on port 3000
+app.listen(3000,() => {
+    console.log('Server listening on port 3000');
+});
